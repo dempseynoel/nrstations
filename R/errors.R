529e9b810de5413d2199701384727b7c0ac01a9b
dempseynoel <noel.dempsey@outlook.com>
2019-01-19 15:54:51 +0000

    Update errors.R

--- R/errors.R
+++ R/errors.R
@@ -2,10 +2,11 @@
 
 #' Report an error with a http request
 #'
-#' @param response The text of the server response.
+#' @param response_text The text of the server response.
 #' @keywords internal
 
 error_status <- function(response_text) {
   error <- xml2::xml_children(response_text)
   error <- xml2::xml_text(xml2::xml_child(error[[2]]))
 }
+
