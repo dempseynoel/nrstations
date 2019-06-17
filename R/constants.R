83d62de12eca33eb993e8cff85215188965a12d2
Noel Dempsey <38509541+dempseynoel@users.noreply.github.com>
2019-06-17 12:21:21 +0100

    Update constants.R

--- R/constants.R
+++ R/constants.R
@@ -1,4 +1,4 @@
 ### Package constants
 
-URL_AUTHENTICATE <- "https://datafeeds.nationalrail.co.uk/authenticate"
-URL_STATIONS_API <- "https://datafeeds.nationalrail.co.uk/api/staticfeeds/4.0/stations.xml"
+URL_AUTHENTICATE <- "https://opendata.nationalrail.co.uk/authenticate"
+URL_STATIONS_API <- "https://opendata.nationalrail.co.uk/api/staticfeeds/4.0/stations"
